0ee604c1f7ea90dd20ca6d50cb01a39f879d3d50
Mukesh Sahay <muksahay@amazon.com>
2022-04-07 00:37:25 +0530

    Fixed linting errors

--- src/datasource/regions.ts
+++ src/datasource/regions.ts
@@ -1,6 +1,13 @@
 import { SelectableValue } from '@grafana/data';
 
-export const standardRegions = ['ap-southeast-1', 'ap-southeast-2', 'eu-central-1', 'eu-west-1', 'us-east-1', 'us-west-2'];
+export const standardRegions = [
+  'ap-southeast-1',
+  'ap-southeast-2',
+  'eu-central-1',
+  'eu-west-1',
+  'us-east-1',
+  'us-west-2',
+];
 
 export const standardRegionOptions: Array<SelectableValue<string>> = standardRegions.map((v) => ({
   value: v,
