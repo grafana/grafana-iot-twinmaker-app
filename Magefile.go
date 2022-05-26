d9984199a214dea14109423f6d32c88cd6e4bbc3
Todd Treece <360020+toddtreece@users.noreply.github.com>
2022-05-26 19:20:23 -0400

    Chore: Remove custom drone config

--- Magefile.go
+++ Magefile.go
@@ -10,20 +10,5 @@ import (
 	build "github.com/grafana/grafana-plugin-sdk-go/build"
 )
 
-// Drone signs the Drone configuration file
-// This needs to be run everytime the drone.yml file is modified
-// See https://github.com/grafana/deployment_tools/blob/master/docs/infrastructure/drone/signing.md for more info
-func Drone() error {
-	if err := sh.RunV("drone", "lint"); err != nil {
-		return err
-	}
-
-	if err := sh.RunV("drone", "--server", "https://drone.grafana.net", "sign", "--save", "grafana/grafana-iot-twinmaker-app"); err != nil {
-		return err
-	}
-
-	return nil
-}
-
 // Default configures the default target.
 var Default = build.BuildAll
