fa795b40df39197eb1289a11d8dd1bd165ac0d43
Todd Treece <todd.treece@grafana.com>
2022-06-17 02:33:53 +0000

    prevent panel refresh on data link clicks

--- src/common/variables.ts
+++ src/common/variables.ts
@@ -8,9 +8,10 @@ interface VariableOptions {
 export function getVariableOptions(opts?: VariableOptions) {
   const templateSrv = getTemplateSrv();
   return templateSrv.getVariables().map((t) => {
-    const value = '${' + t.name + '}';
+    const value = t.name;
+    const label = '${' + t.name + '}';
     const info: SelectableValue<string> = {
-      label: opts?.hideValue ? `${value}` : `${value} = ${templateSrv.replace(value)}`,
+      label: opts?.hideValue ? `${label}` : `${label} = ${templateSrv.replace(label)}`,
       value,
       icon: 'arrow-right', // not sure what makes the most sense
     };
