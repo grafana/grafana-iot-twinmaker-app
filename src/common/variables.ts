789c9ee8f78f5b57458550434a8a9c2aaf5f45f5
Harry Wandersman <hwanders@amazon.com>
2022-07-12 14:35:50 -0400

    Reverting changes for preventing panel refresh on data link clicks

--- src/common/variables.ts
+++ src/common/variables.ts
@@ -8,10 +8,9 @@ interface VariableOptions {
 export function getVariableOptions(opts?: VariableOptions) {
   const templateSrv = getTemplateSrv();
   return templateSrv.getVariables().map((t) => {
-    const value = t.name;
-    const label = '${' + t.name + '}';
+    const value = '${' + t.name + '}';
     const info: SelectableValue<string> = {
-      label: opts?.hideValue ? `${label}` : `${label} = ${templateSrv.replace(label)}`,
+      label: opts?.hideValue ? `${value}` : `${value} = ${templateSrv.replace(value)}`,
       value,
       icon: 'arrow-right', // not sure what makes the most sense
     };
