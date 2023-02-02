d61d568b9f51ac7bcd5558009eed813473937075
Jack Westbrook <jack.westbrook@gmail.com>
2023-02-02 16:39:52 +0100

    test: fix up failing tests due to jest bump and es classes

--- src/panels/video-player/VideoPlayer.test.tsx
+++ src/panels/video-player/VideoPlayer.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { mockGrafanaUI, mockTwinMakerPanelProps, mockTimeRange } from '../tests/utils/__mocks__';
+import { mockTwinMakerPanelProps, mockTimeRange, mockGrafanaUI } from '../tests/utils/__mocks__';
 
 mockGrafanaUI();
-jest.doMock('react-router-dom', () => ({
+jest.mock('react-router-dom', () => ({
   useLocation: () => {
     return { search: '' };
   },
@@ -17,11 +17,28 @@ jest.doMock('@grafana/runtime', () => ({
   locationSearchToObject: mockLocationSearchToObject,
 }));
 
-const videoPlayerMock = jest.fn();
-const requestUploadMock = jest.fn();
-jest.doMock('@iot-app-kit/react-components', () => ({
-  VideoPlayer: videoPlayerMock,
-  RequestVideoUpload: requestUploadMock,
+const mockVideoPlayer = jest.fn();
+const mockRequestUpload = jest.fn();
+
+jest.mock('@iot-app-kit/react-components', () => ({
+  VideoPlayer: class MockVideoPlayer extends React.PureComponent {
+    constructor(props: any) {
+      super(props);
+      mockVideoPlayer(props);
+    }
+    render() {
+      return <div></div>;
+    }
+  },
+  RequestVideoUpload: class MockRequestVideoUpload extends React.PureComponent {
+    constructor(props: any) {
+      super(props);
+      mockRequestUpload(props);
+    }
+    render() {
+      return <div></div>;
+    }
+  },
 }));
 
 import { VideoPlayer } from './VideoPlayer';
@@ -37,17 +54,6 @@ setTemplateSrv({
 
 describe('VideoPlayer', () => {
   it('should load VideoPlayer component when providing kvsStreamName', () => {
-    let videoPlayProp: any;
-    videoPlayerMock.mockImplementation((p) => {
-      videoPlayProp = p;
-      return (<div>VideoPlayer</div>) as any;
-    });
-    let requestUploadProp: any;
-    requestUploadMock.mockImplementation((p) => {
-      requestUploadProp = p;
-      return (<div>RequestVideoUpload</div>) as any;
-    });
-
     const mockKvsStream = 'mockKvsStream';
     const mockEntityId = 'mockEntityId';
     const mockComponentName = 'mockComponentName';
@@ -75,7 +81,7 @@ describe('VideoPlayer', () => {
 
     expect(panelProps.appKitTMDataSource.videoData).toBeCalledTimes(1);
     expect(panelProps.appKitTMDataSource.videoData).toBeCalledWith(options);
-    expect(videoPlayProp).toEqual({ videoData: mockVideoDate, viewport });
-    expect(requestUploadProp).toEqual({ videoData: mockVideoDate });
+    expect(mockVideoPlayer).toHaveBeenCalledWith(expect.objectContaining({ videoData: mockVideoDate, viewport }));
+    expect(mockRequestUpload).toHaveBeenCalledWith(expect.objectContaining({ videoData: mockVideoDate }));
   });
 });
