903b53e7f0b211ccee7bb558bdcb326903dcfd5c
Mohamed Elmuhtadi <moelmu@amazon.com>
2023-02-22 10:07:11 -0800

    Upgrade UxSDK and Fix TwinMaker Query Editor panel hight resize (#170)

--- src/panels/query-editor/QueryEditor.tsx
+++ src/panels/query-editor/QueryEditor.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
 import 'aws-iot-twinmaker-grafana-utils/dist/index.css';
-import { getStyles } from './styles';
+
 import { QueryEditorPropsFromParent } from './interfaces';
-import { QueryEditorWrapper } from 'aws-iot-twinmaker-grafana-utils';
+import { QueryBuilder } from 'aws-iot-twinmaker-grafana-utils';
 
 export const QueryEditor = (props: QueryEditorPropsFromParent) => {
-  const styles = getStyles(props.width);
-  return (
-    <div data-testid={'QueryEditor'} className={styles.wrapper}>
-      <QueryEditorWrapper workspaceId={props.workspaceId} awsConfig={props.awsConfig} />
-    </div>
-  );
+  return <QueryBuilder workspaceId={props.workspaceId} awsConfig={props.awsConfig} />;
 };
