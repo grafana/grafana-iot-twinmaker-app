3af28a9d58c09037068e86400bd491260384743f
Isabella Siu <isabella.siu@grafana.com>
2023-06-14 17:18:37 -0400

    put the semicolon back

--- src/module.ts
+++ src/module.ts
@@ -1,6 +1,6 @@
 import { AppPlugin } from '@grafana/data';
 import { registerXFormer } from 'transformer/RegisterTwinMakerLinksEditor';
 
-export const plugin = new AppPlugin()
+export const plugin = new AppPlugin();
 
 registerXFormer();
