81aa94a4cafc3f0db44a5f3eb7187708d1ec19ed
Isabella Siu <Isabella.siu@grafana.com>
2023-06-15 09:08:13 -0400

    Remove ConfigCtrl (#200)

--- src/module.ts
+++ src/module.ts
@@ -1,8 +1,5 @@
-import { ExampleConfigCtrl } from './legacy/config';
 import { AppPlugin } from '@grafana/data';
 import { registerXFormer } from 'transformer/RegisterTwinMakerLinksEditor';
-
-export { ExampleConfigCtrl as ConfigCtrl };
 
 export const plugin = new AppPlugin();
 
