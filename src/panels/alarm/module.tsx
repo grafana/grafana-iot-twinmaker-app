1ef2354654a267ae9e150174a6ba80ad240212f2
idastambuk <ida.stambuk@grafana.com>
2024-10-18 17:30:57 +0200

    Remove alarm panel

--- src/panels/alarm/module.tsx
+++
@@ -1,5 +0,0 @@
-import { PanelPlugin } from '@grafana/data';
-import { TwinMakerAlarmPanel } from './TwinMakerAlarmPanel';
-import { AlarmPanelOptions } from './types';
-
-export const plugin = new PanelPlugin<AlarmPanelOptions>(TwinMakerAlarmPanel);
