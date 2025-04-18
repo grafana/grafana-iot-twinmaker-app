906e16a3c77e2aab466043c77a37d1dbfdb532bf
idastambuk <ida.stambuk@grafana.com>
2025-04-18 03:52:39 +0200

    make refresh work in 10

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,25 +1,35 @@
 import {
   SceneDataTransformer,
-  sceneGraph,
   SceneObject,
   SceneObjectState,
   SceneQueryRunner,
   VizPanel,
 } from '@grafana/scenes';
+import { lte } from 'semver';
+import { PanelModel } from './dashboard';
+import { config } from '@grafana/runtime';
 
 export function refreshPanelsInDashboard() {
-  const panels = getAllDashboardPanels();
-  panels.forEach((panel) => {
+  if (isGrafanaLte10()) {
+    refreshAngularDashboard();
+  } else {
+    const panels = getAllDashboardPanels();
+    panels.forEach((panel) => {
     const queryRunner = getQueryRunnerFor(panel);
     queryRunner?.runQueries();
-  });
+     });
+  }
 }
 
 function getAllDashboardPanels(): VizPanel[] {
   const sceneContext: SceneObject<SceneObjectState> = (window as any).__grafanaSceneContext;
   if (sceneContext) {
-    return sceneGraph.findAllObjects(sceneContext, (obj) => obj.constructor.name === 'VizPanel') as VizPanel[];
+    import('@grafana/scenes').then((scenes) => {
+      // alarm panel fails to load if sceneGraph is imported at the top level in 10, so it needs to be conditionally imported
+      return scenes.sceneGraph.findAllObjects(sceneContext, (obj) => obj.constructor.name === 'VizPanel') as VizPanel[];
+    })
   } else return [];
+  return [];
 }
 
 export function getQueryRunnerFor(sceneObject: SceneObject | undefined): SceneQueryRunner | undefined {
@@ -39,4 +49,18 @@ export function getQueryRunnerFor(sceneObject: SceneObject | undefined): SceneQu
     }
   }
   return undefined;
+}
+
+function refreshAngularDashboard() {
+  // Grafana will stop plugin from loading in >=12 if we import getLegacyAngularInjector at top level, so have to lazy load it
+  import('@grafana/runtime').then((runtime) => {
+    const $injector = runtime.getLegacyAngularInjector();
+    const dashboardSrv = $injector.get('dashboardSrv');
+    dashboardSrv.dashboard?.panels?.forEach((panel: PanelModel) => panel.refresh());
+  });
+}
+
+function isGrafanaLte10(): boolean {
+  let grafanaVersion = config.buildInfo?.version;
+  return lte(grafanaVersion, '10.4.17');
 }
